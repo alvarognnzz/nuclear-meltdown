682957f10de1195a00d454c979e8d98e8146d217
alvarognnzz <alvgonz.ojeda@gmail.com>
2024-12-27 19:12:46 +0100

    prototype v0.3: fixed seeing obobjects trough walls

--- entities/pickables/pickable_object/pickable_object.gd
+++ entities/pickables/pickable_object/pickable_object.gd
@@ -1,2 +1,10 @@
 class_name Pickable
 extends "res://entities/pickables/pickable.gd"
+
+@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+
+func _physics_process(delta: float) -> void:
+	if picked:
+		mesh_instance_3d.set_layer_mask_value(2, true)
+	else:
+		mesh_instance_3d.set_layer_mask_value(2, false)
