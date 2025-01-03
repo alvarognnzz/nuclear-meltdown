e481911bf7afa513b8865377d2639741316fec24
alvarognnzz <alvgonz.ojeda@gmail.com>
2025-01-03 12:30:03 +0100

    prototype v0.7: add picking support to new interaction

--- entities/pickables/pickable.gd
+++ entities/pickables/pickable.gd
@@ -1,14 +1,9 @@
-extends RigidBody3D
-
-@export var key: String
-@export var input_action: String
-@export var action_name: String
-
-@export var interaction_type: Global.InteractionTypes = Global.InteractionTypes.INSTANT
-@export var in_character_position: Vector3 = Vector3.ZERO
-@export var in_character_rotation: Vector3 = Vector3.ZERO
+class_name Pickable
+extends Node
 
 @export var audio_stream_player: AudioStreamPlayer 
+@export var in_character_position: Vector3
+@export var in_character_rotation: Vector3
 
 var progress_speed = 1
 var picked: bool = false
@@ -19,7 +14,15 @@ var character: CharacterBody3D
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 const fall_multiplier: float = 2.5
 
+var parent: RigidBody3D
+var meshes: Array
+
 func _ready() -> void:
+	if not get_parent() is RigidBody3D:
+		push_error("Pickable.gd: Parent must be a RigidBody3D")
+	
+	parent = get_parent()
+	meshes = get_all_meshes(parent)
 	character = get_tree().get_first_node_in_group("character")
 
 func _input(event: InputEvent) -> void:
@@ -30,24 +33,38 @@ func _input(event: InputEvent) -> void:
 		
 		picked = false
 		character.picking = false
-		reparent(previous_parent)
-		freeze = false
-		global_position = character.global_position
-		global_rotation = Vector3.ZERO
+		parent.reparent(previous_parent)
+		parent.freeze = false
+		parent.global_position = character.global_position
+		parent.global_rotation = Vector3.ZERO
+		
+		for mesh in meshes:
+			mesh.set_layer_mask_value(2, false)
 
-func interact() -> void:
+func start_picking() -> void:
 	if audio_stream_player:
 		audio_stream_player.pitch_scale = randf_range(0.9, 1.1)
 		audio_stream_player.play()
-
-	previous_parent = get_parent()
+	
+	for mesh in meshes:
+		mesh.set_layer_mask_value(2, true)
+	
+	previous_parent = parent.get_parent()
 	picked = true
 	character.picking = true
 	var picking_pivot = character.picking_pivot
-	reparent(picking_pivot)
-	freeze = true
-	position = in_character_position
-	rotation = in_character_rotation
+	parent.reparent(picking_pivot)
+	parent.freeze = true
+	parent.position = in_character_position
+	parent.rotation = in_character_rotation
 
-func can_interact() -> bool:
-	return not character.picking
+func get_all_meshes(node: Node) -> Array:
+	var meshes = []
+	if node is MeshInstance3D:
+		meshes.append(node)
+	for child in node.get_children():
+		meshes += get_all_meshes(child)
+	return meshes
+
+#func can_interact() -> bool:
+	#return not character.picking
--- entities/pickables/pickable_object/pickable_object.gd
+++ entities/pickables/pickable_object/pickable_object.gd
@@ -1,7 +1,12 @@
-class_name Pickable
-extends "res://entities/pickables/pickable.gd"
+extends RigidBody3D
+#class_name Pickable
+#extends "res://entities/pickables/pickable.gd"
+#
+#@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+#
+#func _ready() -> void:
+	#super()
 
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 
-func _ready() -> void:
-	super()
+func _on_interactable_interacted() -> void:
+	$Pickable.start_picking()
